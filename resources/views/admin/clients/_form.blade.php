02f33afda6aaf617f63333db8a2712c4926c69e6
mex8063 <mex8063@24046-5636.INFO>
2019-03-21 13:43:23 -0300

    Variedade de campos do Laravel Collective Forms

--- resources/views/admin/clients/_form.blade.php
+++ resources/views/admin/clients/_form.blade.php
@@ -1,25 +1,23 @@
 {{ csrf_field() }}
 <input type="hidden" name="client_type" value="{{ $clientType }}">
 <div class="form-group">
-    {{ Form::label('name')}}
+    {{ Form::label('name', 'Nome')}}
     {{ Form::text('name', old('name', $client->name), ['class' => 'form-control']) }}
-    <!-- <label for="name">Nome</label>-->
-    <!-- <input type="text" class="form-control" id="name" name="name" value="{{old('name', $client->name)}}"> -->
 </div>
 
 <div class="form-group">
-    <label for="document_number">Documento</label>
-    <input type="text" class="form-control" id="document_number" name="document_number" value="{{ old('document_number', $client->document_number) }}">
+    {{ Form::label('document_number', 'Documento')}}
+    {{ Form::text('document_number', old('document_number', $client->document_number), ['class' => 'form-control']) }}
 </div>
 
 <div class="form-group">
-    <label for="email">E-mail</label>
-    <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $client->email) }}">
+    {{ Form::label('email', 'E-mail')}}
+    {{ Form::email('email', old('email', $client->email), ['class' => 'form-control']) }}
 </div>
 
 <div class="form-group">
-    <label for="phone">Telefone</label>
-    <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone', $client->phone) }}">
+    {{ Form::label('phone', 'E-Telefone')}}
+    {{ Form::text('phone', old('phone', $client->phone), ['class' => 'form-control']) }}
 </div>
 
 
@@ -28,18 +26,20 @@
         $maritalStatus = old('marital_status', $client->marital_status);
     @endphp
     <div class="form-group">
-        <label for="marital_status">Estado Civil</label>
-        <select class="form-control" id="marital_status" name="marital_status" value="{{ $maritalStatus }}">
-            <option value="">Selecione o estado civil</option>
-            <option value="1" {{ $maritalStatus == 1 ? 'selected="selected"': ''}}>Solteiro</option>
-            <option value="2" {{ $maritalStatus == 2 ? 'selected="selected"': ''}}>Casado</option>
-            <option value="3" {{ $maritalStatus == 3 ? 'selected="selected"': ''}}>Divorciado</option>
-        </select>
+        {{ Form::label('marital_status', 'Estado Civil')}}
+        {{
+            Form::select('marital_status', [
+                '' => 'Selecione o estado civil',
+                '1' => 'Solteiro',
+                '2' => 'Casado',
+                '3' => 'Divorciado'
+            ], old('marital_status', $maritalStatus), ['class' => 'form-control'])
+        }}
     </div>
 
     <div class="form-group">
-        <label for="date_birth">Data Nasc.</label>
-        <input type="date" class="form-control" id="date_birth" name="date_birth" value="{{ old('date_birth', $client->date_birth) }}">
+        {{ Form::label('date_birth', 'Data Nasc.')}}
+        {{ Form::date('date_birth', old('date_birth', $client->date_birth), ['class' => 'form-control']) }}
     </div>
 
     @php
@@ -47,30 +47,30 @@
     @endphp
     <div class="radio">
         <label>
-            <input type="radio" name="sex" value="m" {{trim($sex) == 'm' ? 'checked="checked"': ''}}> Masculino
+            {{ Form::radio('sex', 'm', trim($sex) == 'm') }} Masculino
         </label>
     </div>
 
     <div class="radio">
         <label>
-            <input type="radio" name="sex" value="f" {{trim ($sex) == 'f' ? 'checked="checked"': ''}}> Feminino
+            {{ Form::radio('sex', 'f', trim($sex) == 'f') }} Feminino
         </label>
     </div>
 
     <div class="form-group">
-        <label for="physical_disability">Deficiência Física</label>
-        <input type="text" class="form-control" id="physical_disability" name="physical_disability" value="{{ old('physical_disability', $client->physical_disability) }}">
+        {{ Form::label('physical_disability', 'Deficiência Física')}}
+        {{ Form::text('physical_disability', old('physical_disability', $client->physical_disability), ['class' => 'form-control']) }}
     </div>
 
 @else
     <div class="form-group">
-        <label for="company_name">Nome Fantasia</label>
-        <input type="text" class="form-control" id="company_name" name="company_name" value="{{old('company_name', $client->company_name) }}">
+        {{ Form::label('company_name', 'Nome Fantasia')}}
+        {{ Form::text('company_name', old('company_name', $client->company_name), ['class' => 'form-control']) }}
     </div>
 @endif
 
 <div class="checkbox">
     <label>
-        <input type="checkbox" id="defaulter" name="defaulter" {{ old('defaulter', $client->defaulter) ? 'checked="checked"': '' }}> Inadimplente?
+        {{ Form::checkbox('defaulter', 1, old('defaulter', $client->defaulter)) }} Inadimplente?
     </label>
 </div>
