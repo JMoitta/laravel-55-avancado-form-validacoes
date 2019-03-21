18c4428c52a50f275f4ee695d361debfc025ca33
mex8063 <mex8063@24046-5636.INFO>
2019-03-21 09:03:13 -0300

    Helper asset

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,23 +1,15 @@
 <html>
 <head>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"/> <!-- asset('') -->
 </head>
 <body>
 <div class="row">
     <div class="container">
         <h1>Laravel: Formulários e Validações</h1>
-        @if(Session::has('message'))
-            <div class="alert alert-success alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                {{Session::get('message')}}
-            </div>
-        @endif
         @yield('content')
     </div>
 </div>
 
-<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script> <!-- asset('') -->
 </body>
 </html>
